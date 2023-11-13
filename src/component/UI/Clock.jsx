b158ef26e0b6b6a6ec94d4f59532e501af02fd6e
ranveer singh tomar <ranveersinghtomar2000@gmail.com>
2023-11-13 21:38:51 +0530

    again commit

--- src/component/UI/Clock.jsx
+++ src/component/UI/Clock.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 
 const Clock = () => {
+    const [days ,setDays]= useState()
+    const [hours ,setHours]= useState()
+    const [minutes ,setminutes]= useState()
+    const [seconds ,setSeconds]= useState()
+
+    let interval;
+
+    const countDown =()=>{
+        const destination = new Date('dec 10, 2023').getTime()
+
+        interval = setInterval(()=>{
+            const now = new Date().getTime()
+            const different = destination - now
+
+            const days = Math.floor(different / (1000 * 60 * 60 * 24))
+            const hours = Math.floor(different % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ))
+            const minutes = Math.floor(different % (1000 * 60 * 60) / (1000 * 60))
+            const seconds = Math.floor(different % (1000 * 60) / (1000))
+
+            if(destination < 0) clearInterval(interval.current)
+            else{
+                setDays(days)
+                setHours(hours)
+                setminutes(minutes)
+                setSeconds(seconds)
+            }
+        })
+    }
+    useEffect(()=>{
+        countDown()
+    })
   return (
-    <div>Clock</div>
+    <div className="clock__wrapper d-flex align-item-center gap-3">
+        <div className="clock__data d-flex align-item-center gap-3">
+            <div className='text-center'>
+                <h1 className='text-white fs-3 mb-2'>{days}</h1>
+                <h5 className='text-white fs-6'>Days</h5>
+            </div>
+            <span className='text-white fs-3'>
+                :
+            </span>
+        </div>
+        <div className="clock__data d-flex align-item-center gap-3">
+            <div className='text-center'>
+                <h1 className='text-white fs-3 mb-2'>{hours}</h1>
+                <h5 className='text-white fs-6'>Hours</h5>
+            </div>
+            <span className='text-white fs-3'>
+                :
+            </span>
+        </div>
+        <div className="clock__data d-flex align-item-center gap-3">
+            <div className='text-center'>
+                <h1 className='text-white fs-3 mb-2'>{minutes}</h1>
+                <h5 className='text-white fs-6'>Minutes</h5>
+            </div>
+            <span className='text-white fs-3'>
+                :
+            </span>
+        </div>
+        <div className="clock__data d-flex align-item-center gap-3">
+            <div className='text-center'>
+                <h1 className='text-white fs-3 mb-2'>{seconds}</h1>
+                <h5 className='text-white fs-6'>Second</h5>
+            </div>
+        </div>
+    </div>
   )
 }
 
